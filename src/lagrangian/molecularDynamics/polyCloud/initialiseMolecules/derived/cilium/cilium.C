2bbb631dc22f5c01fd579d5532b394ea5801c837
matthew borg <matthew.borg@ed.ac.uk>
2015-10-20 18:50:05 +0100

    more changes

--- src/lagrangian/molecularDynamics/polyCloud/initialiseMolecules/derived/cilium/cilium.C
+++ src/lagrangian/molecularDynamics/polyCloud/initialiseMolecules/derived/cilium/cilium.C
@@ -153,7 +153,7 @@ void cilium::setInitialConfiguration()
         
         if(cell != -1)
         {
-            insertMolecule
+            insertMoleculeLocal
             (
                 positions[i],
                 cell,
@@ -174,13 +174,109 @@ void cilium::setInitialConfiguration()
         << ", no of molecules inserted = " << nMolsInserted
         << endl;
     
+        
     // write out of ordered locations 
-    // IMPORTANT
     
-    
+    forAll(trackingNumbers_, i)
+    {
+        if(i < trackingNumbers_.size() - 1)
+        {
+            trackingNumbersA_.append(trackingNumbers_[i]);
+            trackingNumbersB_.append(trackingNumbers_[i+1]);
+        }
+    }
+
+    Info << "trackingNumbersA = " << trackingNumbersA_ << endl;
+    Info << "trackingNumbersB = " << trackingNumbersB_ << endl;
 }
 
 
+// simple modifications to the insert molecule function
+void cilium::insertMoleculeLocal
+(
+    const point& position,
+    const label cell,
+    const label tetFace,
+    const label tetPt, 
+    const label& id, 
+    const bool& tethered,
+    const bool& frozen,
+    const scalar& temperature,
+    const vector& bulkVelocity
+)
+{
+    point specialPosition(vector::zero);
+
+    label special = 0;
+
+    if (tethered)
+    {
+        specialPosition = position;
+
+        special = polyMolecule::SPECIAL_TETHERED;
+    }
+
+    if (frozen)
+    {
+        specialPosition = position;
+
+        special = polyMolecule::SPECIAL_FROZEN;
+    }
+
+//     const polyMolecule::constantProperties& cP = molCloud_.constProps(id);
+
+    vector v = equipartitionLinearVelocity(temperature, molCloud_.cP().mass(id));
+
+    v += bulkVelocity;
+
+    vector pi = vector::zero;
+
+    tensor Q = I;
+
+    if (!molCloud_.cP().pointMolecule(id))
+    {
+        pi = equipartitionAngularMomentum(temperature, id);
+        scalar phi(molCloud_.rndGen().sample01<scalar>()*constant::mathematical::twoPi);
+        scalar theta(molCloud_.rndGen().sample01<scalar>()*constant::mathematical::twoPi);
+        scalar psi(molCloud_.rndGen().sample01<scalar>()*constant::mathematical::twoPi);
+
+        Q = tensor
+        (
+            cos(psi)*cos(phi) - cos(theta)*sin(phi)*sin(psi),
+            cos(psi)*sin(phi) + cos(theta)*cos(phi)*sin(psi),
+            sin(psi)*sin(theta),
+            - sin(psi)*cos(phi) - cos(theta)*sin(phi)*cos(psi),
+            - sin(psi)*sin(phi) + cos(theta)*cos(phi)*cos(psi),
+            cos(psi)*sin(theta),
+            sin(theta)*sin(phi),
+            - sin(theta)*cos(phi),
+            cos(theta)
+        );
+    }
+    
+    label tNI = molCloud_.getTrackingNumber();
+    trackingNumbers_.append(tNI);
+//     positions_.append(position);
+    
+    molCloud_.createMolecule
+    (
+        position,
+        cell,
+        tetFace,
+        tetPt,     
+        Q,
+        v,
+        vector::zero,
+        pi,
+        vector::zero,
+        specialPosition,
+        special,
+        id,
+        1.0,
+        tNI
+    );
+    
+}
 
 
 
