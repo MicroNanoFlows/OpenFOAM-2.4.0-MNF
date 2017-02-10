8ee16e579b90ac5db2ab6fecbfdac754fa7d38c0
srinivasa <s.ramisetti@ed.ac.uk>
2017-02-10 12:18:46 +0000

    icoMacroFOAM added

---
+++ applications/utilities/preProcessing/molecularDynamics/polyatomics/reconstructXmolWithVelocity/reconstructXmolWithVelocity.C
@@ -0,0 +1,121 @@
+/*---------------------------------------------------------------------------*\
+ =========                   |
+ \\      /   F ield          | OpenFOAM: The Open Source CFD Toolbox
+  \\    /    O peration      |
+   \\  /     A nd            | Copyright (C) 2008-2009 OpenCFD Ltd.
+    \\/      M anipulation   |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Description
+    Reconstructs the .xmol files required for visualisation in VMD, paraView etc.
+
+    For each time-directory the utlitity is used, the cloud is read in from the hard
+    disc and the .xmol file uses the read in positions to write it out.
+
+    Use examples:
+    (for all time directories)
+        > reconstrucXmol 
+
+    (for one time directory, e.g. 12)
+        > reconstrucXmol -time 12
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "mdPoly.H"
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    timeSelector::addOptions();
+
+#   include "setRootCase.H"
+#   include "createTime.H"
+
+    instantList timeDirs = timeSelector::select0(runTime, args);
+
+#   include "createMesh.H"
+#   include "createRandom.H"
+
+    reducedUnits rU(runTime, mesh);
+
+    constantMoleculeProperties cP (mesh, rU);
+        
+    polyMoleculeCloud molecules
+    (
+        runTime,
+        mesh,
+        rU,
+        cP,
+        rndGen,
+        "NULL",
+        false
+    );
+
+    forAll(timeDirs, timeI)
+    {
+        runTime.setTime(timeDirs[timeI], timeI);
+
+        Info<< "Time: " << runTime.timeName() << endl;
+
+        molecules.readNewField();
+
+        fileName path(runTime.path()/runTime.timeName() +  + "/lagrangian" + "/polyMoleculeCloud_velocity.xmol");
+
+	OFstream os(path);
+
+	os  << molecules.nSites() << nl << "polyMoleculeCloud site positions in angstroms, velocities in angstrom/femtosecond" << nl;
+        
+        //molecules.writeXYZ(path);
+
+	IDLList<polyMolecule>::iterator mol(molecules.begin());
+
+        for (mol= molecules.begin(); mol != molecules.end(); ++mol)
+	  {
+// 	    const polyMolecule::constantProperties& cP = molecules.constProps(mol().id());
+
+	    forAll(mol().sitePositions(), i)
+	      {
+		const point& sP = mol().sitePositions()[i];
+		
+		os  << cP.siteNames()[mol().id()][i]
+		    << ' ' << sP.x()*rU.refLength()*1.0e10
+		    << ' ' << sP.y()*rU.refLength()*1.0e10
+		    << ' ' << sP.z()*rU.refLength()*1.0e10
+			<< ' ' << mol().v().x()*rU.refVelocity()*1.0e-5
+		    << ' ' << mol().v().y()*rU.refVelocity()*1.0e-5
+		    << ' ' << mol().v().z()*rU.refVelocity()*1.0e-5
+		    << nl;
+	      }
+	  }
+
+
+        runTime.write();
+    }
+
+    Info<< nl << "ClockTime = " << runTime.elapsedClockTime() << " s"
+        << nl << endl;
+
+    Info << nl << "End\n" << endl;
+    return 0;
+}
+
+// ************************************************************************* //
