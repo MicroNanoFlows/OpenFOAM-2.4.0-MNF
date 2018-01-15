b628b3dac00fc6c67333402e09504d1345cabe3c
S.B.Ramisetti <ramisettisrinivas@yahoo.com>
2018-01-15 16:50:42 +0000

    few more updates

--- applications/solvers/incompressible/hacFOAM/hacFoam.C
+++ applications/solvers/incompressible/hacFOAM/hacFoam.C
@@ -22,7 +22,7 @@ License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 Application
-    icoFoam
+    hacFoam
 
 Description
     Transient solver for incompressible, laminar flow of Newtonian fluids.
