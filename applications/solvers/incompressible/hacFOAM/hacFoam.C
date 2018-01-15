544e12209849024cb2a0610081eb6f3069a3ba25
S.B.Ramisetti <ramisettisrinivas@yahoo.com>
2018-01-15 12:07:49 +0000

    new file added

--- applications/solvers/incompressible/hacFOAM/hacFoam.C
+++ applications/solvers/incompressible/hacFOAM/hacFoam.C
@@ -6,7 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
-    This file is part of OpenFOAM.
+    This file is part of OpenFOAM. 
 
     OpenFOAM is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
@@ -30,10 +30,12 @@ Description
 \*---------------------------------------------------------------------------*/
 
 #include "fvCFD.H"
+#include "ioHelper.H"
 
 #ifdef USE_MUI
     #include "mui.h"
 #endif
+
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -47,41 +49,84 @@ int main(int argc, char *argv[])
     #include "initContinuityErrs.H"
 
 #ifdef USE_MUI
-	if (!args.parRunControl().parRun())
-	{
-		MPI_Init(&argc, &argv);
-	}
+    if (!args.parRunControl().parRun())
+    {
+       MPI_Init(&argc, &argv);
+    }
 #endif
+
+    Info << setprecision(10);
+    reducedUnits redUnits(runTime, mesh);
+
+    dictionary hmmDict =
+    IOdictionary
+    (
+        IOobject
+        (
+            "hmmDict",
+            runTime.system(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    );
+
+    ioHelper ioHelp(runTime, redUnits, hmmDict, mesh);
 
 #ifdef USE_MUI
-    int nMacro=5;
-	Foam::word macro_domainName;
-	macro_domainName="macro_0";
-	std::vector<std::string> interfaceNames;
-	for(label i=0; i < nMacro; i++)
-	{
-		std::string tmp="ifs"+std::to_string(i+1);
-		interfaceNames.push_back(tmp);
-	}
-	MPI_Comm  world = mui::mpi_split_by_app();
-	std::vector<mui::uniface<mui::config_3d>* > cplMacroInterfaces=mui::create_uniface<mui::config_3d>(macro_domainName,interfaceNames);
+    Foam::word macro_domainName;
+    macro_domainName="macro_0";
+    std::vector<std::string> interfaceNames;
+    for (label i=0; i < ioHelp.nMicro(); i++)
+    {
+        std::string tmp="ifs"+std::to_string(i+1);
+        interfaceNames.push_back(tmp);
+    }
 
+    MPI_Comm  world = mui::mpi_split_by_app();
+    std::vector<mui::uniface<mui::config_1d>* > cplMacroInterfaces=mui::create_uniface<mui::config_1d>(macro_domainName,interfaceNames);
 #endif
 
-    scalar nMicro=5;
+    std::map<int,vector> a_2_c_velMap;
+    std::map<int,scalarField> c_2_a_velMap_x;
+    std::map<int,scalarField> c_2_a_velMap_y;
+    vector vel;
+    
     scalar iterationId=0;
-    vector velocity;
-    List<vector> CFD_2_MD_velocities;
-    List<vector> MD_2_CFD_velocities;
-	std::map<int,vector> mVelocityMap;
-	double velocity_SI_2_LAMMPS=1e5;
+
+    List<labelList> &A2C_Cells = ioHelp.cellListA2C();
+    List<labelList> &C2A_Cells = ioHelp.cellListC2A();
+    int nA2CDataPts_p1=A2C_Cells.size();
+    int nC2ADataPts_p1=C2A_Cells.size();
+    int nInterfaces=cplMacroInterfaces.size();
 
     Info<< "\nStarting time loop\n" << endl;
+    /*
+    for (size_t i=0; i < cplMacroInterfaces.size(); i++)
+    {
+        // send forces to LAMMPS HERE
+        string lb_x="phase1_vel_x_";
+	string lb_y="phase1_vel_y_";
+	int lc=std::stoi(cplMacroInterfaces[i]->getIFS().substr(3,1));
+	mui::point1d loc(lc);	
+	double v[3];
+	  
+	v[0]=1.7;
+	//v[1]=c_2_a_velMap_y[lc];
+	
+	cplMacroInterfaces[i]->push(lb_x, loc, v[0]);
+	//cplMacroInterfaces[i]->push(lb_y, loc, v[1]);
+	
+	//Commit (transmit) values to the MUI interface			
+	cplMacroInterfaces[i]->commit(iterationId);
+	std::cout << "\nInitial CFD push vel " << v[0] << " ifs name: " << cplMacroInterfaces[i]->getIFS() << " location: " << lc << " iteration: " << iterationId << std::endl;
+	
+    }
+    */
 
     while (runTime.loop())
     {
-	    #include "adjust_BC.H"
-		Info<< "Time = " << runTime.timeName() << nl << endl;
+        Info<< "Time = " << runTime.timeName() << nl << endl;
 
         #include "readPISOControls.H"
         #include "CourantNo.H"
@@ -134,86 +179,76 @@ int main(int argc, char *argv[])
             U.correctBoundaryConditions();
         }
 
-#ifdef USE_MUI
-
-
-		vector vel;
-		for(size_t i=0; i < cplMacroInterfaces.size(); i++)
-		{
-			int ifsIndex=i+1;
-			// convert force (SI units) to LAMMPS units (Real units)
-			vel.x()=CFD_2_MD_velocities[i].x()*velocity_SI_2_LAMMPS;
-			vel.y()=CFD_2_MD_velocities[i].y()*velocity_SI_2_LAMMPS;
-			vel.z()=CFD_2_MD_velocities[i].z()*velocity_SI_2_LAMMPS;
-			mVelocityMap[ifsIndex]=vel;
-			
-		}
-
-		for(size_t i=0; i < cplMacroInterfaces.size(); i++)
-		{
-			// force=forces[i].x()*1.44e10;
-			// send forces to LAMMPS HERE
-			string lb="velocity_";
-			int lc=std::stoi(cplMacroInterfaces[i]->getIFS().substr(3,1));
-			mui::point3d loc;
-			loc[0]=lc;
-			loc[1]=lc;
-			loc[2]=lc;
-			double v[3];
-			v[0]=mVelocityMap[lc][0];
-			v[1]=mVelocityMap[lc][1];
-			v[2]=mVelocityMap[lc][2];
-			
-			cplMacroInterfaces[i]->push(lb, loc, v[0]);
-			cplMacroInterfaces[i]->push(lb, loc, v[1]);
-			cplMacroInterfaces[i]->push(lb, loc, v[2]);
-			//Commit (transmit) values to the MUI interface			
-			cplMacroInterfaces[i]->commit(iterationId);
-			std::cout << "CFD push " << "ifs : " << lc << "velocity : " << v[0]  << std::endl;
-
-		}
-  
-#endif
+	//scalar current_t = runTime.value();
+        //int count = current_t/5e-13; //some tests !!!
+	//if(current_t>75e-10)
+	{
 
 #ifdef USE_MUI
-		std::map<int,vector> velMap;
 
-		for(label i=0; i < nMicro; i++)
-		{
-			int lc=std::stoi(cplMacroInterfaces[i]->getIFS().substr(3,1));
-			mui::point3d loc;
-			loc[0]=lc; loc[1]=lc; loc[2]=lc;
-			vector v;
-			v.x()=cplMacroInterfaces[i]->fetch("massrate_", loc, iterationId, mui::sampler_exact3d<double>(), mui::chrono_sampler_exact3d());
-			v.y()=cplMacroInterfaces[i]->fetch("massrate_", loc, iterationId, mui::sampler_exact3d<double>(), mui::chrono_sampler_exact3d());
-			v.z()=cplMacroInterfaces[i]->fetch("massrate_", loc, iterationId, mui::sampler_exact3d<double>(), mui::chrono_sampler_exact3d());
-			velMap[lc] = v;
-//			std::cout << "CFD fetch 0 " << "ifs : " << i << " " << lc << "mass flow rate : " << mDotMap[lc]  << " " << massrate <<std::endl;
-		}
+      #include "c_to_a.H"
+      for (size_t i=0; i < cplMacroInterfaces.size(); i++)
+      {
+	  // send velocity to LAMMPS
+	  int lc=std::stoi(cplMacroInterfaces[i]->getIFS().substr(3,1));
+	  mui::point1d loc(lc);	
 
-		label i=0;
-		for(auto const & item : velMap) 
-		{
-			vector v=item.second;
-			MD_2_CFD_velocities[i].x()=v.x();
-			MD_2_CFD_velocities[i].y()=v.y();
-			MD_2_CFD_velocities[i].z()=v.z();
-			//std::cout << "CFD fetch " << "ifs : " << i << " " << item.first << " mass flow rate : " << mDot[i]  << std::endl;
-			i++;
-		}
-		     
+	  scalarField tempX=c_2_a_velMap_x[lc];
+	  scalarField tempY=c_2_a_velMap_y[lc];
+	  for(int j=0; j<tempX.size(); j++)
+	  {
+	    vel[0]=tempX[j];
+	    vel[1]=tempY[j];
+	    cplMacroInterfaces[i]->push("phase1_vel_x_", i*nC2ADataPts_p1+j, vel[0]);
+	    cplMacroInterfaces[i]->push("phase1_vel_y_", i*nC2ADataPts_p1+j, vel[1]);
+	    Info << "\nCFD push vel " << vel << " ifs name: " << cplMacroInterfaces[i]->getIFS() << " location: " << lc << " iteration: " << iterationId << endl;
+	  }
+	  //Commit (transmit) values to the MUI interface
+	  cplMacroInterfaces[i]->commit(iterationId);
+      }
+
+      for (label i=0; i < ioHelp.nMicro(); i++)
+      {
+	for(int j=0;j<nA2CDataPts_p1;j++){
+	  int lc=i*nA2CDataPts_p1+j;
+	  vel[0]=cplMacroInterfaces[i]->fetch("phase1_md_avg_vel_x_", lc, iterationId, mui::sampler_exact1d<double>(), mui::chrono_sampler_exact1d());
+	  vel[1]=cplMacroInterfaces[i]->fetch("phase1_md_avg_vel_y_", lc, iterationId, mui::sampler_exact1d<double>(), mui::chrono_sampler_exact1d());
+	  vel[2]=0.0;
+	  
+	  a_2_c_velMap[lc] = vel;
+	  Info << "\nCFD fetch vel " << vel << " ifs name: " << cplMacroInterfaces[i]->getIFS() << " location: " << lc << " iteration: " << iterationId << endl;
+    
+	}
+      }
+      
+      #include "a_to_c.H"
+      iterationId++;
 #endif
 
+      runTime.write();	  
+      }
 
-        runTime.write();
-
-        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
-            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
-            << nl << endl;
-		iterationId++;
+      Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+	  << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+	  << nl << endl;
     }
 
     Info<< "End\n" << endl;
+
+#ifdef USE_MUI
+    for(size_t i=0; i < cplMacroInterfaces.size(); i++)
+    {
+	delete cplMacroInterfaces[i];
+    }
+#endif
+
+#ifdef USE_MUI
+    if (!args.parRunControl().parRun())
+    {
+	MPI_Finalize();
+    }
+#endif
+
 
     return 0;
 }
