eb49ef3d77217d5d0052b40c68304c875fb5c8b6
craigwhite87 <craig.white.2@glasgow.ac.uk>
2018-06-20 16:45:25 +0100

    Added new dsmcCyclicPatch class

--- src/lagrangian/dsmc/boundaries/derived/cyclicBoundaries/dsmcReflectiveParticleMembranePatch/dsmcReflectiveParticleMembranePatch.C
+++ src/lagrangian/dsmc/boundaries/derived/cyclicBoundaries/dsmcReflectiveParticleMembranePatch/dsmcReflectiveParticleMembranePatch.C
@@ -43,6 +43,8 @@ addToRunTimeSelectionTable(dsmcCyclicBoundary, dsmcReflectiveParticleMembranePat
 void dsmcReflectiveParticleMembranePatch::readProperties()
 {
     p_ = (readScalar(propsDict_.lookup("reflectionProbability")));
+    velocity_ = propsDict_.lookup("velocity");
+    temperature_ = readScalar(propsDict_.lookup("temperature"));
 }
 
 
@@ -60,6 +62,8 @@ dsmcReflectiveParticleMembranePatch::dsmcReflectiveParticleMembranePatch
     dsmcCyclicBoundary(t, mesh, cloud, dict),
     propsDict_(dict.subDict(typeName + "Properties")),
     p_(readScalar(propsDict_.lookup("reflectionProbability"))),
+    velocity_(propsDict_.lookup("velocity")),
+    temperature_(readScalar(propsDict_.lookup("temperature"))),
     nReflections_(0),
     nRejections_(0)
 
@@ -107,13 +111,17 @@ void dsmcReflectiveParticleMembranePatch::initialConfiguration()
 
 void dsmcReflectiveParticleMembranePatch::controlMol
 (
-    dsmcParcel& mol,
+    dsmcParcel& p,
     dsmcParcel::trackingData& td
 )
 {
-    const label& faceI = mol.face();
+    const label& faceI = p.face();
 
     vector nF = mesh_.faceAreas()[faceI];
+    vector nw = p.normal();
+    
+    vector& U = p.U();
+    
 //     const vector& fC  = mesh_.faceCentres()[faceI];
 //     const label f = findIndex(controlPatch(), faceI);
 
@@ -122,16 +130,26 @@ void dsmcReflectiveParticleMembranePatch::controlMol
 //     label f = findIndex(faces_, faceI);
     label fA = findIndex(coupledFacesA_, faceI);
     label fB = findIndex(coupledFacesB_, faceI);
-
+    
     nF /= mag(nF);
+    nw /= mag(nw);
+    
+    scalar U_dot_nw = U & nw;
+    vector Ut = U - U_dot_nw*nw;
+    
+    label typeId = p.typeId();
+    
+    const scalar& T = temperature_;
+
+    scalar mass = cloud_.constProps(typeId).mass();
 
     Random& rndGen = cloud_.rndGen();
 
-    scalar d = nF & mol.U();
+    scalar d = nF & U;
 
 /*            Info<< "parcel to reflect at pos: " 
-                << mol.position() << ", nF: " << nF
-                << " old velocity: " << mol.U() 
+                << p.position() << ", nF: " << nF
+                << " old velocity: " << p.U() 
                 << " faceI: " << faceI
                 << " fA: " << fA
                 << " fB: " << fB
@@ -147,24 +165,63 @@ void dsmcReflectiveParticleMembranePatch::controlMol
 
             if( pRandom <= p_ ) // reflect molecule
             {
-                scalar Un = mol.U() & nF;
+                //scalar Un = U & nF;
     
-                mol.U() -= 2.0*Un*nF;
+                //U -= 2.0*Un*nF;
+                
+                while (mag(Ut) < SMALL)
+                {
+                    // If the incident velocity is parallel to the face normal, no
+                    // tangential direction can be chosen.  Add a perturbation to the
+                    // incoming velocity and recalculate.
+
+                    U = vector
+                    (
+                        U.x()*(0.8 + 0.2*rndGen.scalar01()),
+                        U.y()*(0.8 + 0.2*rndGen.scalar01()),
+                        U.z()*(0.8 + 0.2*rndGen.scalar01())
+                    );
+
+                    U_dot_nw = U & nw;
+
+                    Ut = U - U_dot_nw*nw;
+                }
+
+                // Wall tangential unit vector
+                vector tw1 = Ut/mag(Ut);
+
+                // Other tangential unit vector
+                vector tw2 = nw^tw1;
+
+                U =
+                    sqrt(physicoChemical::k.value()*T/mass)
+                *(
+                        rndGen.GaussNormal()*tw1
+                    + rndGen.GaussNormal()*tw2
+                    - sqrt(-2.0*log(max(1 - rndGen.scalar01(), VSMALL)))*nw
+                    );
+                
+                scalar Un = U & nF;
+    
+                U -= 2.0*Un*nF;
+                
+//                 p.position() -= Un*nF*0.01*mesh_.time().deltaTValue();
+                
+                U += velocity_;
 
                 td.switchProcessor = false;
 
                 nReflections_++;
 
-//                 Pout<< "Reflected!!: mol at pos: " 
-//                     << mol.position() << ", nF: " << nF
-//                     << " tracking number: " << mol.trackingNumber()
-//                     << " new velocity: " << mol.v()
+//                 Pout<< "Reflected!!: p at pos: " 
+//                     << p.position() << ", nF: " << nF
+//                     << " tracking number: " << p.trackingNumber()
+//                     << " new velocity: " << p.v()
 //                     << endl;
 
             }
             else
             {
-
                 nRejections_++;
             }
         }
@@ -173,9 +230,9 @@ void dsmcReflectiveParticleMembranePatch::controlMol
     {
         if(fB != -1)
         {    
-//             Info<< " to reflect mol at pos: " 
-//                 << mol.position() << ", nF: " << nF
-//                 << " old velocity: " << mol.U() 
+//             Info<< " to reflect p at pos: " 
+//                 << p.position() << ", nF: " << nF
+//                 << " old velocity: " << p.U() 
 //                 << " faceI: " << faceI
 //                 << " fA: " << fA
 //                 << " fB: " << fB
@@ -185,20 +242,59 @@ void dsmcReflectiveParticleMembranePatch::controlMol
 
             if( pRandom <= p_ ) // reflect molecule
             {
-                scalar Un = mol.U() & nF;
+                //scalar Un = U & nF;
     
-                mol.U() -= 2.0*Un*nF;
+                //U -= 2.0*Un*nF;
+                
+                while (mag(Ut) < SMALL)
+                {
+                    // If the incident velocity is parallel to the face normal, no
+                    // tangential direction can be chosen.  Add a perturbation to the
+                    // incoming velocity and recalculate.
+
+                    U = vector
+                    (
+                        U.x()*(0.8 + 0.2*rndGen.scalar01()),
+                        U.y()*(0.8 + 0.2*rndGen.scalar01()),
+                        U.z()*(0.8 + 0.2*rndGen.scalar01())
+                    );
+
+                    U_dot_nw = U & nw;
+
+                    Ut = U - U_dot_nw*nw;
+                }
+
+                // Wall tangential unit vector
+                vector tw1 = Ut/mag(Ut);
+
+                // Other tangential unit vector
+                vector tw2 = nw^tw1;
+
+                U =
+                    sqrt(physicoChemical::k.value()*T/mass)
+                *(
+                        rndGen.GaussNormal()*tw1
+                    + rndGen.GaussNormal()*tw2
+                    - sqrt(-2.0*log(max(1 - rndGen.scalar01(), VSMALL)))*nw
+                    );
+                
+                scalar Un = U & nF;
+    
+                U -= 2.0*Un*nF;
+                
+//                 p.position() -= Un*nF*0.01*mesh_.time().deltaTValue();
+                
+                U += velocity_;
 
                 nReflections_++;
 
-//                 Info<< "Reflected!!: mol at pos: " 
-//                     << mol.position() 
-//                     << " new velocity: " << mol.U()
+//                 Info<< "Reflected!!: p at pos: " 
+//                     << p.position() 
+//                     << " new velocity: " << p.U()
 //                     << endl;
             }
             else
             {
-
                 nRejections_++;
             }
         }
