8fa033fa7d82f43a9277e7169b395bd2cea2dfbb
Stephen Longshaw <stephen.longshaw@stfc.ac.uk>
2020-05-15 17:51:16 +0100

    Add new null electrostatic potential

---
+++ src/lagrangian/molecularDynamics/polyCloud/potentials/pairPotentials/derived/electrostaticPotentialModels/noElectrostatic/noElectrostatic.C
@@ -0,0 +1,95 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2007 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Description
+
+\*---------------------------------------------------------------------------*/
+
+#include "noElectrostatic.H"
+
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+defineTypeNameAndDebug(noElectrostatic, 0);
+addToRunTimeSelectionTable(pairPotentialModel, noElectrostatic, dictionary);
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+// Construct from components
+noElectrostatic::noElectrostatic
+(
+    const polyMesh& mesh,
+    polyMoleculeCloud& molCloud,
+    const reducedUnits& redUnits,
+    const word& name, 
+    const dictionary& dict
+)
+:
+    pairPotentialModel(mesh, molCloud, redUnits, name, dict),
+    propsDict_(dict.subDict(typeName + "Coeffs"))
+{
+}
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+noElectrostatic::~noElectrostatic()
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+scalar noElectrostatic::unscaledEnergy(const scalar r) const
+{
+    return 0;
+}
+
+
+
+scalar noElectrostatic::force(const scalar r) const
+{
+    return 0;
+}
+    
+scalar noElectrostatic::energy(const scalar r) const
+{
+    return 0;
+}
+
+const dictionary& noElectrostatic::dict() const
+{
+    return pairPotentialProperties_;
+}
+
+void noElectrostatic::write(const fileName& pathName)
+{
+}
+
+} // End namespace Foam
+
+// ************************************************************************* //
