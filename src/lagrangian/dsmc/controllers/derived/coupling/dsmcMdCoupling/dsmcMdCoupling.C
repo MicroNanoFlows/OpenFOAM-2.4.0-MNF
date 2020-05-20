f6a52220efb81251b6dc802c526bd0a0c2d5e930
Stephen Longshaw <stephen.longshaw@stfc.ac.uk>
2020-05-20 17:08:55 +0100

    Bugfix

--- src/lagrangian/dsmc/controllers/derived/coupling/dsmcMdCoupling/dsmcMdCoupling.C
+++ src/lagrangian/dsmc/controllers/derived/coupling/dsmcMdCoupling/dsmcMdCoupling.C
@@ -762,7 +762,7 @@ void dsmcMdCoupling::sendCoupledRegion(bool init)
     // Reset ghost status for each parcel
     for(parc = cloud_.begin(); parc != cloud_.end(); ++parc)
     {
-        parc.setGhost(false);
+        parc.setAsGhost(false);
     }
 
     dsmcParcel* parcel = NULL;
@@ -825,7 +825,7 @@ void dsmcMdCoupling::sendCoupledRegion(bool init)
 
                             parcelsInCellHistory_[iface].append(parcel->origId()); //Store the parcel ID that was sent
 
-                            parcel->setGhost(true); //Set this parcel as being ghosted
+                            parcel->setAsGhost(true); //Set this parcel as being ghosted
                         }
                     }
                 }
